343ed5596d58c5c01fe04dc8d49ba95e13ec0c3b
Jourzy <emmanora@hotmail.com>
2024-11-15 12:22:26 +0000

    Added route table and association for application subnet.

--- terraform/route.tf
+++ terraform/route.tf
@@ -21,6 +21,30 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+
+# route table for application subnets
+resource "aws_route_table" "application" {
+ vpc_id = aws_vpc.main.id
+ 
+# Routes all traffic through the internet gateway
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.main.id # Route through IG
+ }
+ 
+ tags = {
+   Name = "Application subnet route table"
+ }
+}
+
+# Associate application route table with application subnets
+resource "aws_route_table_association" "application" {
+  count          = length(aws_subnet.application)
+  subnet_id      = element(aws_subnet.application[*].id, count.index)
+  route_table_id = aws_route_table.application.id
+}
+
+
 # route table for backend subnets
 # Routes all traffic through the nat gateway
 resource "aws_route_table" "backend" {
