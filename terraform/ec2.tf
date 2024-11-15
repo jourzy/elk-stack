639e1ba1eb3a5c346605ee9d4fec33ac0f6be177
Jourzy <emmanora@hotmail.com>
2024-11-15 13:22:03 +0000

    Added new instances for web-server, logstash, kibana and elasticsearch.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -44,3 +44,55 @@ resource "aws_instance" "bastion" {
     Name = "Bastion instance"
   }
 }
+
+
+
+###################################
+
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = ["${aws_security_group.application.id}"]
+  associate_public_ip_address = true
+
+  subnet_id                   = aws_subnet.application[0].id
+  
+  tags                        = { Name = "Web Server" }
+}
+
+resource "aws_instance" "kibana_instance" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = ["${aws_security_group.public.id}"]
+  associate_public_ip_address = true
+
+  subnet_id                   = aws_subnet.public[0].id
+
+  tags                        = { Name = "Kibana Instance" }
+}
+
+resource "aws_instance" "logstash_instance" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = ["${aws_security_group.private.id}"]
+  associate_public_ip_address = true
+
+  subnet_id                   = aws_subnet.private[0].id
+
+  tags                        = { Name = "Logstash Instance" }
+}
+
+resource "aws_instance" "elasticsearch_instance" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair_name
+  vpc_security_group_ids      = ["${aws_security_group.private.id}"]
+  associate_public_ip_address = true
+
+  subnet_id                   = aws_subnet.private[0].id
+
+  tags                        = { Name = "Elasticsearch Instance" }
+}
