7dd462f6f4ba9e0390d36bd10301a7ad46976661
Jourzy <emmanora@hotmail.com>
2024-11-15 13:24:51 +0000

    Commented out instances not using but keeping in case I need multiple instances for high availability so i can reuse code.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,39 +15,36 @@ data "aws_ami" "ubuntu" {
   
 }
 
-resource "aws_instance" "private" {
-  count                       = length(aws_subnet.private)
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
-  key_name                    = var.key_pair_name
-  vpc_security_group_ids      = ["${aws_security_group.private.id}"]
-  associate_public_ip_address = false
+# resource "aws_instance" "private" {
+#   count                       = length(aws_subnet.private)
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = var.instance_type
+#   key_name                    = var.key_pair_name
+#   vpc_security_group_ids      = ["${aws_security_group.private.id}"]
+#   associate_public_ip_address = false
   
-  subnet_id                   = element(aws_subnet.private[*].id, count.index)
+#   subnet_id                   = element(aws_subnet.private[*].id, count.index)
 
-  tags = {
-    Name = "Private instance ${count.index + 1}"
-  }
-}
+#   tags = {
+#     Name = "Private instance ${count.index + 1}"
+#   }
+# }
 
 
-resource "aws_instance" "bastion" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
-  key_name                    = var.key_pair_name
-  vpc_security_group_ids      = ["${aws_security_group.public.id}"]
-  associate_public_ip_address = true
+# resource "aws_instance" "bastion" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = var.instance_type
+#   key_name                    = var.key_pair_name
+#   vpc_security_group_ids      = ["${aws_security_group.public.id}"]
+#   associate_public_ip_address = true
   
-  subnet_id                   = aws_subnet.public[0].id
+#   subnet_id                   = aws_subnet.public[0].id
 
-  tags = {
-    Name = "Bastion instance"
-  }
-}
+#   tags = {
+#     Name = "Bastion instance"
+#   }
+# }
 
-
-
-###################################
 
 resource "aws_instance" "web_server" {
   ami                         = data.aws_ami.ubuntu.id
