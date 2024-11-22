6dae16a29b1514347aa98b97b7003bf40bbd5f1b
Jourzy <emmanora@hotmail.com>
2024-11-22 13:14:36 +0000

    Added proper route tables and associations.

--- terraform/route.tf
+++ terraform/route.tf
@@ -1,70 +1,46 @@
-
-# route table for public subnets
+# Public route table for public subnet
 resource "aws_route_table" "public" {
- vpc_id = aws_vpc.main.id
- 
-# Routes all traffic through the internet gateway
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.main.id # Route through IG
- }
- 
- tags = {
-   Name = "Public subnet route table"
- }
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "elk-public-route-table"
+  }
 }
 
-# Associate public route table with public subnets
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
 
-# route table for application subnets
-resource "aws_route_table" "application" {
- vpc_id = aws_vpc.main.id
- 
-# Routes all traffic through the internet gateway
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.main.id # Route through IG
- }
- 
- tags = {
-   Name = "Application subnet route table"
- }
-}
 
-# Associate application route table with application subnets
-resource "aws_route_table_association" "application" {
-  count          = length(aws_subnet.application)
-  subnet_id      = element(aws_subnet.application[*].id, count.index)
-  route_table_id = aws_route_table.application.id
-}
-
-
-# route table for backend subnets
-# Routes all traffic through the nat gateway
-resource "aws_route_table" "backend" {
-  count  = length(aws_nat_gateway.nat)
+# Private route table (for backend and application subnets)
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.nat[*].id, count.index)  # Route through NAT
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = {
-    Name = "Backend route table"
+    Name = "elk-private-route-table"
   }
 }
 
 
-# Associate backend route table with backend subnets
+# Route table associations for private subnets
 resource "aws_route_table_association" "backend" {
-  count          = length(aws_subnet.backend)
-  subnet_id      = element(aws_subnet.backend[*].id, count.index)
-  route_table_id = element(aws_route_table.backend[*].id, count.index)
+  subnet_id      = aws_subnet.backend.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "application" {
+  subnet_id      = aws_subnet.application.id
+  route_table_id = aws_route_table.private.id
 }
