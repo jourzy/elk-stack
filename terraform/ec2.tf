2965a6ac6a5e91312af490c111db10a37e2245bf
Jourzy <emmanora@hotmail.com>
2024-11-25 22:16:51 +0000

    Updated ec2 instances with correct instance types, security groups etc.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,81 +15,51 @@ data "aws_ami" "ubuntu" {
   
 }
 
-# resource "aws_instance" "private" {
-#   count                       = length(aws_subnet.private)
-#   ami                         = data.aws_ami.ubuntu.id
-#   instance_type               = var.instance_type
-#   key_name                    = var.key_pair_name
-#   vpc_security_group_ids      = ["${aws_security_group.private.id}"]
-#   associate_public_ip_address = false
-  
-#   subnet_id                   = element(aws_subnet.private[*].id, count.index)
 
-#   tags = {
-#     Name = "Private instance ${count.index + 1}"
-#   }
-# }
-
-
-# resource "aws_instance" "bastion" {
-#   ami                         = data.aws_ami.ubuntu.id
-#   instance_type               = var.instance_type
-#   key_name                    = var.key_pair_name
-#   vpc_security_group_ids      = ["${aws_security_group.public.id}"]
-#   associate_public_ip_address = true
-  
-#   subnet_id                   = aws_subnet.public[0].id
-
-#   tags = {
-#     Name = "Bastion instance"
-#   }
-# }
-
-
-resource "aws_instance" "web_server" {
+resource "aws_instance" "application" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
+  instance_type               = "t2.micro"
   key_name                    = var.key_pair_name
   vpc_security_group_ids      = ["${aws_security_group.application.id}"]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
-  subnet_id                   = aws_subnet.application[0].id
+  subnet_id                   = aws_subnet.application.id
   
-  tags                        = { Name = "Web Server" }
+  tags                        = { Name = "application-server" }
 }
 
-resource "aws_instance" "kibana_instance" {
+resource "aws_instance" "kibana" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
+  instance_type               = t2.micro
   key_name                    = var.key_pair_name
-  vpc_security_group_ids      = ["${aws_security_group.public.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.kibana.id}"]
   associate_public_ip_address = true
 
-  subnet_id                   = aws_subnet.public[0].id
+  subnet_id                   = aws_subnet.public.id
 
-  tags                        = { Name = "Kibana Instance" }
+  tags                        = { Name = "kibana-server" }
 }
 
-resource "aws_instance" "logstash_instance" {
+resource "aws_instance" "logstash" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
+  instance_type               = t2.micro
   key_name                    = var.key_pair_name
-  vpc_security_group_ids      = ["${aws_security_group.private.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.logstash.id}"]
   associate_public_ip_address = true
 
-  subnet_id                   = aws_subnet.private[0].id
+  subnet_id                   = aws_subnet.private.id
 
-  tags                        = { Name = "Logstash Instance" }
+  tags                        = { Name = "logstash-server" }
 }
 
-resource "aws_instance" "elasticsearch_instance" {
+resource "aws_instance" "elasticsearch" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
+  instance_type               = t3.small
   key_name                    = var.key_pair_name
-  vpc_security_group_ids      = ["${aws_security_group.private.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.elasticsearch.id}"]
   associate_public_ip_address = true
 
-  subnet_id                   = aws_subnet.private[0].id
+  subnet_id                   = aws_subnet.private.id
 
-  tags                        = { Name = "Elasticsearch Instance" }
+  tags                        = { Name = "elasticsearch-server" }
 }
